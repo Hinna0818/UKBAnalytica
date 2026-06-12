acf73356d46cc62223cb00cd1ec2990e4e191bea
Hinna0818 <hinna@i.smu.edu.cn>
2026-06-12 14:07:34 +0800

    attach

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+.onAttach <- function(libname, pkgname) {
+  version <- tryCatch(
+    as.character(utils::packageVersion(pkgname)),
+    error = function(e) NA_character_
+  )
+  version_label <- if (!is.na(version)) paste0(" ", version) else ""
+
+  packageStartupMessage(
+    paste0(
+      "UKBAnalytica", version_label,
+      ": RAP-oriented UK Biobank workflows. ",
+      "Run ukb_check_rap_env() before RAP data extraction."
+    )
+  )
+}
